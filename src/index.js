a292c6a61d05f749b1f32a2f14f82691cc569a68
Chance Smith <chancesmithb@gmail.com>
2018-03-23 09:09:58 -0500

    adds sample icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { APPICONS } from "./constants-icons";
+import { APPICONS, NAVICONS, GRAPHICONS } from "./constants-icons";
 import Icon from "./Icon";
 
 const styles = {
@@ -10,7 +10,35 @@ const styles = {
 
 const App = () => (
   <div style={styles}>
+    <h1>App Icons</h1>
     <Icon icon={APPICONS.ACTIVITY} />
+    <Icon icon={APPICONS.ADD} />
+    <Icon icon={APPICONS.ADD_SMALL} />
+    <Icon icon={APPICONS.ADD_SMALL2} />
+    <Icon icon={APPICONS.ADMIN} />
+    <Icon icon={APPICONS.ARROW_LEFT} />
+    <Icon icon={APPICONS.ARROW_RIGHT} />
+    <h1>Nav Icons</h1>
+    <Icon icon={NAVICONS.ACTIVITY} />
+    <Icon icon={NAVICONS.ADMIN} />
+    <Icon icon={NAVICONS.BULK_ACTIONS} />
+    <Icon icon={NAVICONS.DATA_IMPORT} />
+    <Icon icon={NAVICONS.DATA_IMPORT} />
+    <Icon icon={NAVICONS.HELP} />
+    <Icon icon={NAVICONS.LOGO} />
+    <Icon icon={NAVICONS.REPORTS} />
+    <Icon icon={NAVICONS.REQUESTS} />
+    <Icon icon={NAVICONS.RESEARCH} />
+    <Icon icon={NAVICONS.STORIES} />
+    <Icon icon={NAVICONS.SUGGESTIONS} />
+    <Icon icon={NAVICONS.WORKSPACES} />
+    <h1>Graph Icons</h1>
+    <Icon icon={GRAPHICONS.FILE_BYTES} />
+    <Icon icon={GRAPHICONS.GEO_NLOC} />
+    <Icon icon={GRAPHICONS.HASH_MD5} />
+    <Icon icon={GRAPHICONS.HASH_SHA1} />
+    <Icon icon={GRAPHICONS.HASH_SHA256} />
+    <Icon icon={GRAPHICONS.HASH_SHA512} />
   </div>
 );
 
